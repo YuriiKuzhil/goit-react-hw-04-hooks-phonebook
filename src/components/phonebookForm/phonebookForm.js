b98468eb0f14b03811aee84a42b302409d22194f
Yurii <yuriikuzhil@gmail.com>
2022-01-16 10:51:03 +0200

    Reworked file phonebookForm with use hooks

--- src/components/phonebookForm/phonebookForm.js
+++ src/components/phonebookForm/phonebookForm.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-// import { FaUser, FaPhoneAlt } from 'react-icons/fa';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import {
@@ -11,70 +10,72 @@ import {
   PhoneIcon,
 } from './phonebookForm.styled';
 
-class PhonebookForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function PhonebookForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  addName = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  addContact = e => {
+  const addContact = e => {
     e.preventDefault();
 
     const contact = {
       id: nanoid(5),
-      name: this.state.name,
-      number: this.state.number,
+      name,
+      number,
     };
 
-    this.props.onSubmit(contact);
-
-    this.setState({ name: '', number: '' });
+    onSubmit(contact);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
+  return (
+    <Form onSubmit={addContact}>
+      <Label>
+        Name
+        <Input
+          onChange={handleChange}
+          value={name}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <UserIcon />
+      </Label>
+      <Label>
+        Phone Number
+        <Input
+          onChange={handleChange}
+          value={number}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <PhoneIcon />
+      </Label>
 
-    return (
-      <Form onSubmit={this.addContact}>
-        <Label>
-          Name
-          <Input
-            onChange={this.addName}
-            value={name}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <UserIcon />
-        </Label>
-        <Label>
-          Phone Number
-          <Input
-            onChange={this.addName}
-            value={number}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <PhoneIcon />
-        </Label>
-
-        <Button type="submit">Add contact</Button>
-      </Form>
-    );
-  }
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
 }
 
 PhonebookForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-export default PhonebookForm;
