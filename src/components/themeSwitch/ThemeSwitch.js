a43c5e74f80d8a91ebcb16be1ed6224bd498a088
Yurii <yuriikuzhil@gmail.com>
2022-01-16 11:09:13 +0200

    Updated file ThemeSwitch with use hooks

--- src/components/themeSwitch/ThemeSwitch.js
+++ src/components/themeSwitch/ThemeSwitch.js
@@ -1,49 +1,42 @@
 import { Text, Wrapper, Label } from './ThemeSwitch.styled';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Switch from 'react-switch';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-class ThemeSwitch extends Component {
-  state = { checked: false };
+export default function ThemeSwitch({ themeChange }) {
+  const [checked, setChecked] = useState(false);
 
-  componentDidMount() {
-    const themeStyle = localStorage.getItem('themeStyle');
-
-    themeStyle === 'dark'
-      ? this.setState({ checked: true })
-      : this.setState({ checked: false });
-  }
-
-  handleChange = checked => {
-    this.setState({ checked });
-    this.props.themeChange(checked);
+  const handleChange = checked => {
+    setChecked(checked);
+    themeChange(checked);
   };
+  useEffect(() => {
+    const themeStyle = localStorage.getItem('themeStyle');
+    themeStyle === 'dark' ? setChecked(true) : setChecked(false);
+  }, []);
 
-  render() {
-    return (
-      <Label>
-        <Switch
-          checked={this.state.checked}
-          onChange={this.handleChange}
-          height={30}
-          width={65}
-          offColor="#c05805"
-          onColor="#cdbea7"
-          uncheckedIcon={<Text>DAY MODE</Text>}
-          checkedIcon={<Text>NIGHT MODE</Text>}
-          uncheckedHandleIcon={
-            <Wrapper>
-              <FaSun />
-            </Wrapper>
-          }
-          checkedHandleIcon={
-            <Wrapper>
-              <FaMoon />
-            </Wrapper>
-          }
-        />
-      </Label>
-    );
-  }
+  return (
+    <Label>
+      <Switch
+        checked={checked}
+        onChange={handleChange}
+        height={30}
+        width={65}
+        offColor="#c05805"
+        onColor="#cdbea7"
+        uncheckedIcon={<Text>DAY MODE</Text>}
+        checkedIcon={<Text>NIGHT MODE</Text>}
+        uncheckedHandleIcon={
+          <Wrapper>
+            <FaSun />
+          </Wrapper>
+        }
+        checkedHandleIcon={
+          <Wrapper>
+            <FaMoon />
+          </Wrapper>
+        }
+      />
+    </Label>
+  );
 }
-export default ThemeSwitch;
